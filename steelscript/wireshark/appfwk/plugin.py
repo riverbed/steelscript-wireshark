b4717ed5298c6145fa205c6a1c8fa00532c74bc7
Mike Garabedian <mike.garabedian@riverbed.com>
2015-08-28 14:49:00 -0400

    2015

--- steelscript/wireshark/appfwk/plugin.py
+++ steelscript/wireshark/appfwk/plugin.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014 Riverbed Technology, Inc.
+# Copyright (c) 2015 Riverbed Technology, Inc.
 #
 # This software is licensed under the terms and conditions of the MIT License
 # accompanying the software ("License").  This software is distributed "AS IS"
@@ -13,7 +13,7 @@ class Plugin(AppsPlugin):
     title = 'Wireshark plugin'
     description = 'Extract metrics from pcap files'
     version = pkg_resources.get_distribution('steelscript.wireshark').version
-    author = 'Christopher J. White'
+    author = 'Riverbed Technology'
 
     enabled = True
     can_disable = True
